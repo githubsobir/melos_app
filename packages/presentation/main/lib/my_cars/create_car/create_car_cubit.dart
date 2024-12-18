1c8f690f70c872ca84dcadb75ac177d8332ee7c1
Buronov Nurdiyor <xoybek7@gmail.com>
2024-12-18 12:22:56 +0500

    added cubit

--- packages/presentation/main/lib/my_cars/create_car/create_car_cubit.dart
+++ packages/presentation/main/lib/my_cars/create_car/create_car_cubit.dart
@@ -1,11 +1,31 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 class CreateCarCubit extends Cubit<CreateCarState> {
   CreateCarCubit() : super(CreateCarInitial());
+  int position = 0;
+  changePosition(int position) {
+    this.position = position;
+    emit(PagerState(position));
+  }
 }
 
 @immutable
-sealed class CreateCarState {}
+sealed class CreateCarState extends Equatable {
+  const CreateCarState();
+}
 
-final class CreateCarInitial extends CreateCarState {}
+final class CreateCarInitial extends CreateCarState {
+  @override
+  List<Object?> get props => [];
+}
+
+final class PagerState extends CreateCarState {
+  final int position;
+
+  const PagerState(this.position);
+
+  @override
+  List<Object> get props => [position];
+}
--- packages/presentation/main/lib/my_cars/create_car/create_car_info_screen.dart
+++ packages/presentation/main/lib/my_cars/create_car/create_car_info_screen.dart
@@ -1,12 +1,18 @@
 import 'package:common/base_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intent_launcher/intent_launcher.dart';
+import 'package:main/my_cars/create_car/create_car_cubit.dart';
 import 'package:main/my_cars/create_car/page_1.dart';
 import 'package:main/my_cars/create_car/page_2.dart';
 import 'package:main/my_cars/create_car/page_3.dart';
 import 'package:main/my_cars/create_car/page_4.dart';
 
 class CreateCarInfoScreen extends StatelessWidget {
-  const CreateCarInfoScreen({super.key});
+  CreateCarInfoScreen({super.key});
+
+  final PageController controller = PageController(initialPage: 0);
+  final CreateCarCubit cubit = CreateCarCubit();
 
   @override
   Widget build(BuildContext context) {
@@ -14,56 +20,94 @@ class CreateCarInfoScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Мои машины"),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: PageView(
-              children: [
-                Page1(),
-                Page2(),
-                Page3(),
-                Page4()
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 16,
-              right: 16,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () {},
-                  child: Card(
-                    elevation: 0,
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                        side: BorderSide(
-                            color: Theme.of(context).colorScheme.primary)),
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          left: 16, right: 16, top: 6, bottom: 6),
-                      child: Text(
-                        "Оставлять",
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                              color: Theme.of(context).colorScheme.primary,
-                            ),
+      body: BlocBuilder(
+        bloc: cubit,
+        builder: (context, state) {
+          return Column(
+            children: [
+              Expanded(
+                child: PageView(
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  controller: controller,
+                  onPageChanged: (position) {
+                    cubit.changePosition(position);
+                  },
+                  children: [
+                    Page1(),
+                    Page2(),
+                    Page3(),
+                    Page4(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        cubit.changePosition(--cubit.position);
+                        if (cubit.position == -1) {
+                          context.closeActiveScreen();
+                        } else {
+                          controller.animateToPage(
+                            cubit.position,
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.linear,
+                          );
+                        }
+                      },
+                      child: Card(
+                        elevation: 0,
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            side: BorderSide(
+                                color: Theme.of(context).colorScheme.primary)),
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 16, right: 16, top: 6, bottom: 6),
+                          child: Text(
+                            cubit.position == 0 ? "Оставлять" : "Назад",
+                            textAlign: TextAlign.center,
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium!
+                                .copyWith(
+                                  color: Theme.of(context).colorScheme.primary,
+                                ),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    BaseButton(
+                        onPressed: () {
+                          cubit.changePosition(++cubit.position);
+                          if (cubit.position <= 3) {
+                            controller.animateToPage(
+                              cubit.position,
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.linear,
+                            );
+                          } else {
+                            // context.openScreen(LoginIntent());
+                          }
+                        },
+                        title: "Следующий")
+                  ],
                 ),
-                BaseButton(onPressed: () {}, title: "Следующий")
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 52,
-          ),
-        ],
+              ),
+              const SizedBox(
+                height: 52,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
