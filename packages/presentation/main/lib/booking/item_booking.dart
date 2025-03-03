89b362e7ce8166b91c02c51b2a6061a5fb54785a
Buronov Nurdiyor <xoybek7@gmail.com>
2025-03-03 17:42:41 +0500

    added some ui !!!

--- packages/presentation/main/lib/booking/item_booking.dart
+++ packages/presentation/main/lib/booking/item_booking.dart
@@ -183,245 +183,118 @@ class ItemBooking extends StatelessWidget {
                   Text(
                     "Отчеты",
                     style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                      fontWeight: FontWeight.w700,
-                    ),
+                          fontWeight: FontWeight.w700,
+                        ),
                   ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            const SizedBox(
-                              height: 12,
-                            ),
-                            ItemTopBottom(
-                              leftText: context.translations.model,
-                              rightText: "Malibu 2 Turbo",
-                            ),
-                            const SizedBox(
-                              height: 12,
-                            ),
-                            ItemTopBottom(
-                              leftText: "Гос. номер",
-                              rightText: "01/A123BC/UZ",
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            const SizedBox(
-                              height: 12,
-                            ),
-                            Text(
-                              "Владелец",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelMedium
-                                  ?.copyWith(
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .secondary),
-                            ),
-                            SizedBox(
-                              height: 12,
-                            ),
-                            Row(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(24.0),
-                                  child: CachedNetworkImage(
-                                    imageUrl:
-                                    "$BASE_URL_IMAGE/backend/media/car_photos/db/cars_image/photo73_kvkGvlK.jpg",
-                                    width: 24,
-                                    height: 24,
-                                    fit: BoxFit.cover,
-                                    progressIndicatorBuilder:
-                                        (context, url, downloadProgress) =>
-                                        SizedBox(
-                                          width: 24,
-                                          height: 24,
-                                          child: Stack(
-                                            children: [
-                                              Align(
-                                                alignment: Alignment.center,
-                                                child: SizedBox(
-                                                  width: 24,
-                                                  height: 24,
-                                                  child: CircularProgressIndicator(
-                                                    value:
-                                                    downloadProgress.progress,
-                                                    color: Theme.of(context)
-                                                        .colorScheme
-                                                        .primary,
-                                                    strokeWidth: 1,
-                                                  ),
-                                                ),
-                                              )
-                                            ],
-                                          ),
-                                        ),
-                                    errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 8,
-                                ),
-                                Text(
-                                  "Azizbek Karimov",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge
-                                      ?.copyWith(
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 12),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 24,
-                            ),
-                            Row(
-                              children: [
-                                SvgPicture.asset(PathImages.phoneCircled),
-                                const SizedBox(
-                                  width: 8,
-                                ),
-                                Text(
-                                  "+998 93 935 0321",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge
-                                      ?.copyWith(
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 12,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  )
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  ItemTopBottom(
+                    leftText: "Стоимость за день",
+                    rightText: "99 000 сум",
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  ItemTopBottom(
+                    leftText: "Залог",
+                    rightText: "9900 сум",
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  ItemTopBottom(
+                    leftText: "Итоговая стоимость",
+                    rightText: "109 990 сум",
+                  ),
                 ],
               ),
             ),
           ),
-
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      context.translations.payment_details,
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                            fontWeight: FontWeight.w700,
-                          ),
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    ItemTopBottom(
-                      leftText: context.translations.deposit,
-                      rightText: "80 000 сум/день",
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    ItemTopBottom(
-                      leftText: context.translations.the_remaining,
-                      rightText: "\$44.00",
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    ItemTopBottom(
-                      leftText: context.translations.total_payment_amount,
-                      rightText: "\$99.00",
-                    ),
-                  ],
-                ),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Подробности аренды",
+                    style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                          fontWeight: FontWeight.w700,
+                        ),
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  ItemTopBottom(
+                    leftText: "Дата получения",
+                    rightText: "12 нояб. 2025 г.",
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  ItemTopBottom(
+                    leftText: "Число",
+                    rightText: "18 нояб. 2025 г.",
+                  ),
+                ],
               ),
-              const SizedBox(
-                width: 24,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      context.translations.booking_status,
-                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                            fontWeight: FontWeight.w700,
-                          ),
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          context.translations.status,
-                          style: Theme.of(context)
-                              .textTheme
-                              .labelMedium
-                              ?.copyWith(
-                                  color:
-                                      Theme.of(context).colorScheme.secondary),
+            ),
+          ),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Статус бронирования",
+                    style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                          fontWeight: FontWeight.w700,
                         ),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Container(
-                          decoration: Decorations.basicDecoration(
-                            background: const Color(0xFF16ED38),
-                            radius: 4,
-                          ),
-                          padding: const EdgeInsets.only(
-                            left: 6,
-                            right: 6,
-                          ),
-                          child: Text(
-                            context.translations.completed,
-                            style: Theme.of(context)
-                                .textTheme
-                                .labelMedium
-                                ?.copyWith(color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Container(
-                      decoration: Decorations.basicDecoration(
-                          background: Theme.of(context).colorScheme.primary,
-                          radius: 4),
-                      padding: const EdgeInsets.only(
-                        left: 6,
-                        right: 6,
-                      ),
-                      child: Text(
-                        context.translations.extend_booking_time,
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        context.translations.status,
                         style: Theme.of(context)
                             .textTheme
                             .labelMedium
-                            ?.copyWith(color: Colors.white),
+                            ?.copyWith(
+                                color: Theme.of(context).colorScheme.secondary),
                       ),
-                    ),
-                  ],
-                ),
+                      SizedBox(
+                        width: 48,
+                      ),
+                      Container(
+                        decoration: Decorations.basicDecoration(
+                          background: const Color(0xFF16ED38),
+                          radius: 4,
+                        ),
+                        padding: const EdgeInsets.only(
+                          left: 24,
+                          right: 24,
+                          top: 8,
+                          bottom: 8,
+                        ),
+                        child: Text(
+                          context.translations.completed,
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelMedium
+                              ?.copyWith(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ],
       ),
