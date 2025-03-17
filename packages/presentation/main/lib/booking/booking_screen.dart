3d27577ea10abc3a865df2f8f2f9b160aa3bf57c
Buronov Nurdiyor <xoybek7@gmail.com>
2025-03-17 09:53:43 +0500

    added tabs

--- packages/presentation/main/lib/booking/booking_screen.dart
+++ packages/presentation/main/lib/booking/booking_screen.dart
@@ -2,8 +2,22 @@ import 'package:common/l10n/build_context_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:main/booking/item_booking.dart';
 
-class BookingScreen extends StatelessWidget {
-  const BookingScreen({super.key});
+class BookingScreen extends StatefulWidget {
+  BookingScreen({super.key});
+
+  @override
+  State<BookingScreen> createState() => _BookingScreenState();
+}
+
+class _BookingScreenState extends State<BookingScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +28,50 @@ class BookingScreen extends StatelessWidget {
               : const Color(0xFF061136),
       appBar: AppBar(
         title: Text(context.translations.booking),
-      ),
-      body: ListView.separated(
-        itemBuilder: (context, index) => const ItemBooking(),
-        separatorBuilder: (context, index) => const Divider(
-          color: Color(0xFF658DF1),
+        toolbarHeight: 72,
+        bottom: TabBar(
+          controller: tabController,
+          padding: const EdgeInsets.all(16),
+          dividerColor: Colors.transparent,
+          indicatorSize: TabBarIndicatorSize.tab,
+          labelStyle: Theme.of(context).textTheme.titleMedium?.copyWith(
+                fontWeight: FontWeight.w700,
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 12,
+              ),
+          unselectedLabelStyle:
+              Theme.of(context).textTheme.titleMedium?.copyWith(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 12,
+                  ),
+          tabs: const [
+            Tab(
+              text: "ТЕКУЩИЕ",
+            ),
+            Tab(
+              text: "ВСЕ",
+            ),
+          ],
         ),
-        itemCount: 3,
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: [
+          ListView.separated(
+            itemBuilder: (context, index) => const ItemBooking(),
+            separatorBuilder: (context, index) => const Divider(
+              color: Color(0xFF658DF1),
+            ),
+            itemCount: 3,
+          ),
+          ListView.separated(
+            itemBuilder: (context, index) => const ItemBooking(),
+            separatorBuilder: (context, index) => const Divider(
+              color: Color(0xFF658DF1),
+            ),
+            itemCount: 3,
+          )
+        ],
       ),
     );
   }
