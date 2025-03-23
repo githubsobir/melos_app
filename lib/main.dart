c3f00c8c8aa2ce16e7e195cef3a6465012339d88
Microstar <xoybek7@gmail.com>
2025-03-23 22:58:53 +0500

    firebase messaging is added!!!!

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,9 @@ import 'dart:io';
 import 'package:carbnb/firebase_options.dart';
 import 'package:dependency/dependencies.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 import 'app/app.dart';
 import 'dependencies.dart';
@@ -15,18 +14,12 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-  if (kDebugMode) {
-    await Permission.notification.isDenied.then((value) {
-      if (value) {
-        Permission.notification.request();
-      }
-    });
-  }
   HttpOverrides.global = MyHttpOverrides();
   configureDependencies(const DependencyConfigurationContext());
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.instance.requestPermission();
   runApp(const MyApp());
 }
 
