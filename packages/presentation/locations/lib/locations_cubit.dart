ce98ea8fb4e48b4b1e79af2e9df1494dee114b64
Buronov Nurdiyor <xoybek7@gmail.com>
2025-03-24 17:28:47 +0500

    removed geocoder

--- packages/presentation/locations/lib/locations_cubit.dart
+++ packages/presentation/locations/lib/locations_cubit.dart
@@ -3,7 +3,6 @@ import 'package:domain/usecase/cars_usecase.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:yandex_geocoder/yandex_geocoder.dart' as g;
 import 'package:yandex_maps_mapkit_lite/init.dart' as init;
 import 'package:yandex_maps_mapkit_lite/mapkit.dart';
 
@@ -21,7 +20,6 @@ class LocationsCubit extends Cubit<LocationsState> {
           ),
         );
   final CarsUseCase _carsUseCase;
-  g.YandexGeocoder? geocoder;
 
   Future<void> gpsList() async {
     emit(state.copyWith(isLoading: true));
@@ -40,20 +38,6 @@ class LocationsCubit extends Cubit<LocationsState> {
     } catch (e) {
       print(e);
     }
-
-    g.GeocodeResponse? geocodeFromPoint = await geocoder?.getGeocode(
-      g.ReverseGeocodeRequest(
-        pointGeocode: (
-          lat: state.point.latitude,
-          lon: state.point.longitude,
-        ),
-      ),
-    );
-    emit(
-      state.copyWith(
-        locationName: geocodeFromPoint?.firstAddress.toString(),
-      ),
-    );
 
     var response = await _carsUseCase.gpsList(
       latitude: state.point.latitude,
@@ -77,9 +61,7 @@ class LocationsCubit extends Cubit<LocationsState> {
     if (response.success) {
       var gps = response.body;
       if (gps != null) {
-        // await init.initMapkit(apiKey: gps);
-        geocoder =
-            g.YandexGeocoder(apiKey: "973005bb-3cfc-4e46-81d2-26939d2b8c3c");
+        await init.initMapkit(apiKey: gps);
         await init.initMapkit(apiKey: "973005bb-3cfc-4e46-81d2-26939d2b8c3c");
       }
     }
--- packages/presentation/main/lib/payment_details/receiving_the_car/receiving_the_car_cubit.dart
+++ packages/presentation/main/lib/payment_details/receiving_the_car/receiving_the_car_cubit.dart
@@ -1,7 +1,7 @@
-import 'package:bloc/bloc.dart';
 import 'package:domain/model/contracts/contract_model.dart';
 import 'package:domain/usecase/contracts_usecase.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ReceivingTheCarCubit extends Cubit<ReceivingTheCarState> {
