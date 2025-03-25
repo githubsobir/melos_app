ab285c0cabe61f664d9d442a607c4e635edf9299
Buronov Nurdiyor <xoybek7@gmail.com>
2025-03-25 16:36:14 +0500

    !!!!!!!!!!!

--- packages/data/lib/models/remote/auth/request/fcm_register_register_request.dart
+++ packages/data/lib/models/remote/auth/request/fcm_register_register_request.dart
@@ -2,24 +2,20 @@ class FcmRegisterRegisterRequest {
   FcmRegisterRegisterRequest({
     this.token,
     this.platform,
-    this.user,
   });
 
   FcmRegisterRegisterRequest.fromJson(dynamic json) {
     token = json['token'];
     platform = json['platform'];
-    user = json['user'];
   }
 
   String? token;
   String? platform;
-  String? user;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
     map['token'] = token;
     map['platform'] = platform;
-    map['user'] = user;
     return map;
   }
 }
